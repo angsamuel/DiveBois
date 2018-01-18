5e736289d8ee8b32baee5c756248cd9d388cb8e8
astralsquid <samuel.ang.prog@gmail.com>
2018-01-18 09:43:08 -0600

    Added main menu components

---
+++ Assets/StatsPanel.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StatsPanel : MonoBehaviour {
+	int combat = 2;int psychology = 2; int management = 2; int computers = 2; int medicine = 2; int mechanics = 2;
+	int credits = 5;
+
+    List<int> stats;
+	public List<Text> statLabels;
+	public List<string> grades;
+
+	public Text creditsText;
+
+
+	void Start(){
+		stats = new List<int> (){ combat, psychology, management, computers, medicine, mechanics };
+	}
+
+	void Update(){
+
+	}
+
+	void UpdateUI(){
+		stats = new List<int> (){ combat, psychology, management, computers, medicine, mechanics };
+		for (int i = 0; i < stats.Count; i++) {
+			statLabels [i].text = grades [stats [i]];
+		}
+		creditsText.text = credits.ToString();
+	}
+
+	public void TickStat(ref int stat, int num){
+		int newStat = stat + num;
+		int newCredits = credits - num;
+
+		if (newStat >= 0 && newStat <= 5 && newCredits >= 0) {
+			stat = newStat;
+			credits = newCredits;
+			UpdateUI ();
+
+		}
+	}
+
+	void TickCombat(int num){
+		TickStat (ref combat, num);
+	}
+	void TickPsychology(int num){
+		TickStat (ref psychology, num);
+	}
+	void TickManagement(int num){
+		TickStat (ref management, num);
+	}
+	void TickComputers(int num){
+		TickStat (ref computers, num);
+	}
+	void TickMedicine(int num){
+		TickStat (ref medicine, num);
+	}
+	void TickMechanics(int num){
+		TickStat (ref mechanics, num);
+	}
+
+}
---
+++ Assets/TraitsPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraitsPanel : MonoBehaviour {
+
+	List<string> traits = new List<string>(){
+		"Goof Ball",
+		"
+
+
+
+	};
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
